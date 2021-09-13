1941e6c924f7065f12ce9b14e624953b44d9e584
bainaryho <rkswl233@naver.com>
2021-09-13 20:09:16 +0900

    syntax04 ver0.4

--- syntax04.go
+++ syntax04.go
@@ -9,7 +9,7 @@ import (
 //소수는 1과 자기자신 이외에는 나누어 떨어지지 않는수
 func main() {
 
-	//소수 판정 프로그램 ver. 0.6
+	//소수 판정 프로그램 ver. 0.4
 	seed := time.Now().Unix()
 	rand.Seed(seed)
 
@@ -20,35 +20,14 @@ func main() {
 	for i := 2; i < number; i++ { //1과 number일때 반복문이 돌지않음
 		if number%i == 0 {
 			isPrime = false
-			break //첫번째 약수가 발견되면 반복문 즉시 종료(성능향상)
 		}
-		//fmt.Print(i, " ")
+		fmt.Print(i, " ")
 	}
 	if isPrime { //비교 연산자 필요없음
 		fmt.Println(number, "는 소수입니다")
 	} else {
 		fmt.Println(number, "는 소수가 아닙니다")
 	}
-
-	// //소수 판정 프로그램 ver. 0.4
-	// seed := time.Now().Unix()
-	// rand.Seed(seed)
-
-	// isPrime := true
-	// number := rand.Intn(150) + 2 //2~151사이의 수
-	// fmt.Println("임의로 추출된 수 : ", number)
-
-	// for i := 2; i < number; i++ { //1과 number일때 반복문이 돌지않음
-	// 	if number%i == 0 {
-	// 		isPrime = false
-	// 	}
-	// 	fmt.Print(i, " ")
-	// }
-	// if isPrime { //비교 연산자 필요없음
-	// 	fmt.Println(number, "는 소수입니다")
-	// } else {
-	// 	fmt.Println(number, "는 소수가 아닙니다")
-	// }
 
 	//소수 판정 프로그램 ver. 0.3
 	// seed := time.Now().Unix()
